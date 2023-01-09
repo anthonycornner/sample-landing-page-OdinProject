463ea7f48d381d3982a2d529853cf20ba740b680
anthonycornner <cornneranthony@gmail.com>
2023-01-08 19:41:44 -1000

    Add mouse trailer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
 window.onload = function () {
-    let toggleDropDown = document.getElementById("dropdown-btn");
-    let menuList = document.getElementById('dropdown-menu');
-    let links = document.querySelectorAll('.link');
+    const trailer = document.getElementById('trailer');
+
+    const animateTrailer = (e, interacting) => {
+        const x = e.clientX - trailer.offsetWidth/2,
+            y = e.clientY - trailer.offsetHeight/2;
+        
+
+        const keyframes = {
+            transform: `translate(${x}px, ${y}px) scale(${interacting ? 5 : 1})`  
+        }
+
+        trailer.animate(keyframes, {
+            duration: 800,
+            fill: "forwards"
+        });
+        
+    }
+
+    window.onmousemove = e => {
+        const interactable = e.target.closest(".interactable");
+
+            interacting = interactable !== null;
+
+
+        animateTrailer(e, interacting);
+    }
+
+    const toggleDropDown = document.getElementById("dropdown-btn");
+    const menuList = document.getElementById('dropdown-menu');
+    const links = document.querySelectorAll('.link');
     
     console.log(menuList);
     console.log(toggleDropDown);
